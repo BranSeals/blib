9f0e5cc640ece9b157ff0bd9bcc0ab70cb7c2ec7
BranSeals <brandon.seals@gmail.com>
2022-11-12 18:29:53 -0500

    Add shortcut function to add and commit everything
    To support this, I refactored the parameters to allow for a path to be
    given rather than rely entirely on overriding `REPO_PATH`. This should
    give it more utility in scripts that would perform git actions in
    multiple places rather than a single repo.

--- bgit.py
+++ bgit.py
@@ -13,7 +13,7 @@ from blib import bcli
 from blib import berr
 from blib import bfile
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Bran Seals"
 __copyright__ = "Copyright 2022"
 __email__ = "bran.seals.dev@gmail.com"
@@ -22,26 +22,56 @@ __status__ = "Testing"
 REPO_PATH = pathlib.Path(pathlib.Path().home() / "Desktop" / "errors")
 
 
-def git_init(repo: pathlib.PosixPath) -> None:
-    """Initialize a git repo at the filepath if not already created."""
+def git_init(repo_path: pathlib.PosixPath = None) -> None:
+    """Initialize a git repository if absent.
+    """
     global REPO_PATH
-    bfile.create_dir_if_missing(REPO_PATH)
-    bcli.run(['git', '-C', REPO_PATH, 'init'])
+    repo = repo_path or REPO_PATH
+
+    bfile.create_dir_if_missing(repo)
+    if not pathlib.Path(repo, '.git').exists():
+        bcli.run(["git", "-C", repo, "init"])
 
 
-def git_add(file: pathlib.PosixPath) -> None:
-    """Add filepath to a given git repository."""
+def git_add(
+    file: pathlib.PosixPath = ".",
+    repo_path: pathlib.PosixPath = None
+) -> None:
+    """Stage changes in a git repository.
+    """
     global REPO_PATH
-    bfile.create_dir_if_missing(REPO_PATH)
-    cmd = bcli.run(['git', '-C', REPO_PATH, 'add', str(file)])
-    if 'fatal' in cmd.stdout:
-        berr.report_error(f'git add failed: {str(file)}')
+    repo = repo_path or REPO_PATH
+
+    bfile.create_dir_if_missing(repo)
+    cmd = bcli.run(["git", "-C", repo, "add", str(file)])
+    if "fatal" in cmd.stdout:
+        berr.report_error(f"git add failed: {str(file)}")
 
 
-def git_commit(message: str = f"automated commit by {bcli.SCRIPT_NAME}") -> None:
-    """Commit to a git repository set by repo_dir."""
+def git_commit(
+    message: str = f"auto-commit by {bcli.SCRIPT_NAME}",
+    repo_path: pathlib.PosixPath = None,
+) -> None:
+    """Commit changes to a git repository.
+    """
     global REPO_PATH
-    bfile.create_dir_if_missing(REPO_PATH)
-    cmd = bcli.run(['git', '-C', REPO_PATH, 'commit', '-m', message])
-    if 'nothing to commit' in cmd.stdout:
-        berr.report_error('git commit failed')
+    repo = repo_path or REPO_PATH
+
+    bfile.create_dir_if_missing(repo)
+    cmd = bcli.run(["git", "-C", repo, "commit", "-m", message])
+    if "nothing to commit" in cmd.stdout:
+        berr.report_error("git commit failed")
+
+
+def git_add_all_and_commit(
+    message: str = f"auto-commit by {bcli.SCRIPT_NAME}",
+    repo_path: pathlib.PosixPath = None,
+) -> None:
+    """Stage and commit all changes to a git repository.
+    """
+    global REPO_PATH
+    repo = repo_path or REPO_PATH
+
+    git_init(repo)
+    git_add('.', repo)
+    git_commit(f'all changes committed by by {bcli.SCRIPT_NAME}', repo)
