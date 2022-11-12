38882d49806f3713def9a6e1a4a876183bead98c
BranSeals <brandon.seals@gmail.com>
2022-11-11 22:12:07 -0500

    Add metadata to empty scripts

--- bgit.py
+++ bgit.py
@@ -3,3 +3,9 @@
 
 """Handle automated git actions.
 """
+
+__version__ = "0.0.0"
+__author__ = "Bran Seals"
+__copyright__ = "Copyright 2022"
+__email__ = "bran.seals.dev@gmail.com"
+__status__ = "Dev"
