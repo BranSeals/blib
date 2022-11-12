a91b1dc8659ff9c671514831d47ca543c00872bb
BranSeals <brandon.seals@gmail.com>
2022-11-12 17:56:25 -0500

    Refactor path to be overridden
    Calling scripts can set `bgit.REPO_PATH` so it doesn't need to be
    specified as an argument each time. If the value isn't overridden, a git
    repo will be created on the Desktop in an `errors` folder so I'll know
    something is broken and which script it's broken in based on the commit
    message.

--- bgit.py
+++ bgit.py
@@ -2,37 +2,46 @@
 # -*- coding: utf-8 -*-
 
 """Handle automated git actions.
+
+Override repository directory within calling scripts using `bgit.REPO_PATH`.
+If one is not given, an error folder will be created on the user's Desktop.
 """
 
 import pathlib
 
 from blib import bcli
 from blib import berr
+from blib import bfile
 
 __version__ = "1.0.0"
 __author__ = "Bran Seals"
 __copyright__ = "Copyright 2022"
 __email__ = "bran.seals.dev@gmail.com"
-__status__ = "Production"
+__status__ = "Testing"
+
+REPO_PATH = pathlib.Path(pathlib.Path().home() / "Desktop" / "errors")
 
 
-def git_init(repo_path: pathlib.PosixPath) -> None:
+def git_init(repo: pathlib.PosixPath) -> None:
     """Initialize a git repo at the filepath if not already created."""
-    bcli.create_dir_if_missing(repo_path)
-    bcli.run(['git', '-C', repo_path, 'init'])
+    global REPO_PATH
+    bfile.create_dir_if_missing(REPO_PATH)
+    bcli.run(['git', '-C', REPO_PATH, 'init'])
 
 
-def git_add(repo_path: pathlib.PosixPath, file: pathlib.PosixPath) -> None:
+def git_add(file: pathlib.PosixPath) -> None:
     """Add filepath to a given git repository."""
-    bcli.create_dir_if_missing(repo_path)
-    cmd = bcli.run(['git', '-C', repo_path, 'add', str(file)])
+    global REPO_PATH
+    bfile.create_dir_if_missing(REPO_PATH)
+    cmd = bcli.run(['git', '-C', REPO_PATH, 'add', str(file)])
     if 'fatal' in cmd.stdout:
         berr.report_error(f'git add failed: {str(file)}')
 
 
-def git_commit(repo_path: pathlib.PosixPath, message: str = 'backup iA Writer') -> None:
+def git_commit(message: str = f"automated commit by {bcli.SCRIPT_NAME}") -> None:
     """Commit to a git repository set by repo_dir."""
-    bcli.create_dir_if_missing(repo_path)
-    cmd = bcli.run(['git', '-C', repo_path, 'commit', '-m', message])
+    global REPO_PATH
+    bfile.create_dir_if_missing(REPO_PATH)
+    cmd = bcli.run(['git', '-C', REPO_PATH, 'commit', '-m', message])
     if 'nothing to commit' in cmd.stdout:
         berr.report_error('git commit failed')
