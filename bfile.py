86db35121033b6d11176e065781890122fa4d99b
BranSeals <brandon.seals@gmail.com>
2022-11-12 20:26:10 -0500

    Add more file functions from other scripts
    Additional:
        * create_dir_if_missing now returns a bool for more utility

--- bfile.py
+++ bfile.py
@@ -14,7 +14,7 @@ __email__ = "bran.seals.dev@gmail.com"
 __status__ = "Dev"
 
 
-def get_file_list(filepath: pathlib.PosixPath) -> list[str]:
+def get_file_list_str(filepath: pathlib.PosixPath) -> list[str]:
     """Return list of files in a given directory.
 
     Questions:
@@ -23,7 +23,30 @@ def get_file_list(filepath: pathlib.PosixPath) -> list[str]:
     return os.listdir(filepath)
 
 
-def create_dir_if_missing(directory: pathlib.PosixPath) -> None:
-    """Create a directory if one does not exist."""
+def listdir(path: str) -> list[pathlib.PosixPath]:
+    """List full filepaths from a directory in pathlib format.
+
+    TODO: How is this different from abovein terms of usefulness? Test it.
+    """
+    filenames = os.listdir(path)
+    filepaths = []
+    if filenames:
+        for f in filenames:
+            filepaths.append(pathlib.Path(path, f))
+    return filepaths
+
+
+def create_dir_if_missing(directory: pathlib.PosixPath) -> bool:
+    """Create a directory if one does not exist.
+
+    Returns True if directory was created."""
     if not pathlib.Path(directory).exists():
         pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+        return True
+    else:
+        return False
+
+
+def load_content(filepath: pathlib.PosixPath) -> str:
+    with open(filepath, 'r') as f:
+        return f.read()
