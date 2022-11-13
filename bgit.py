36698820d3c1e5e32292e03c54c12c4a3b3600fc
BranSeals <brandon.seals@gmail.com>
2022-11-12 20:27:22 -0500

    Refactor parameters to use kwargs
    This makes it easier to use multiple arguments with default values,
    since one or more may be provided and the normal default parameters
    can't account for this due to its ordering requirements.

--- bgit.py
+++ bgit.py
@@ -22,56 +22,50 @@ __status__ = "Testing"
 REPO_PATH = pathlib.Path(pathlib.Path().home() / "Desktop" / "errors")
 
 
-def git_init(repo_path: pathlib.PosixPath = None) -> None:
+def git_init(**kwargs) -> None:
     """Initialize a git repository if absent.
     """
     global REPO_PATH
-    repo = repo_path or REPO_PATH
+    repo = kwargs.get('repo') or REPO_PATH
 
-    bfile.create_dir_if_missing(repo)
-    if not pathlib.Path(repo, '.git').exists():
+    if (
+        bfile.create_dir_if_missing(repo)
+        or not pathlib.Path(repo, '.git').exists()
+    ):
         bcli.run(["git", "-C", repo, "init"])
 
 
-def git_add(
-    file: pathlib.PosixPath = ".",
-    repo_path: pathlib.PosixPath = None
-) -> None:
+def git_add(**kwargs) -> None:
     """Stage changes in a git repository.
     """
     global REPO_PATH
-    repo = repo_path or REPO_PATH
+    repo = kwargs.get('repo') or REPO_PATH
+    file = kwargs.get('file') or '.'
 
-    bfile.create_dir_if_missing(repo)
     cmd = bcli.run(["git", "-C", repo, "add", str(file)])
     if "fatal" in cmd.stdout:
         berr.report_error(f"git add failed: {str(file)}")
 
 
-def git_commit(
-    message: str = f"auto-commit by {bcli.SCRIPT_NAME}",
-    repo_path: pathlib.PosixPath = None,
-) -> None:
-    """Commit changes to a git repository.
+def git_commit(**kwargs) -> None:
+    """Commit changes in a git repository.
     """
     global REPO_PATH
-    repo = repo_path or REPO_PATH
+    repo = kwargs.get('repo') or REPO_PATH
+    message = kwargs.get('message') or f"auto-commit by {bcli.SCRIPT_NAME}"
 
-    bfile.create_dir_if_missing(repo)
     cmd = bcli.run(["git", "-C", repo, "commit", "-m", message])
     if "nothing to commit" in cmd.stdout:
         berr.report_error("git commit failed")
 
 
-def git_add_all_and_commit(
-    message: str = f"auto-commit by {bcli.SCRIPT_NAME}",
-    repo_path: pathlib.PosixPath = None,
-) -> None:
-    """Stage and commit all changes to a git repository.
+def git_add_all_and_commit(**kwargs) -> None:
+    """Shortcut to stage and commit all changes to a git repository.
     """
     global REPO_PATH
-    repo = repo_path or REPO_PATH
+    repo = kwargs.get('repo') or REPO_PATH
+    message = kwargs.get('message') or None
 
-    git_init(repo)
-    git_add('.', repo)
-    git_commit(f'all changes committed by by {bcli.SCRIPT_NAME}', repo)
+    git_init(repo=repo)
+    git_add(repo=repo)
+    git_commit(message=message, repo=repo)
